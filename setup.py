50ac33d9135bdc8bebce71e102fc45219f6651d9
Amund Hov <amund.hov@gmail.com>
2020-03-15 14:21:07 +0100

    Update setup.py with proper data

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='expense_classifier',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      description='The expense classifier to keep personal finances up to date. While having fun.',
+      url='http://github.com/amundhov/expense_classifier',
+      author='Amund Hov',
+      author_email='amund.hov@gmail.com',
       license='MIT',
       packages=['expense_classifier'],
       scripts='expense_classifier/predict_ofx',
